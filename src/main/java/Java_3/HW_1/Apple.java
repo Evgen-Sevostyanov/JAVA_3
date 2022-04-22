4da8f63c1a41d482d4dedec9ced6e870092216e1
geekbrains <Sea_1994@inbox.ru>
2022-04-23 00:19:20 +0700

    HW1

---
+++ src/main/java/Java_3/HW_1/Apple.java
@@ -0,0 +1,14 @@
+package Java_3.HW_1;
+
+
+public class Apple extends Fruit {
+
+        Apple() {
+            super(1.0f);
+        }
+
+        @Override
+        public String toString() {
+            return "яблоки";
+        }
+    }
